abc8b6ed91b0ad83b5f037ac9d2bc17c48f4d8eb
xuezhiyao <1162586634@qq.com>
2015-04-07 14:42:09 +0800

    ll

---
+++ fdnq/src/fdnaq.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+public class fdnaq {
+
+	 //使用递归方法
+	 private int getFibo(int i) {
+		 if(i==0)
+			 return 0;
+	  if (i == 1 || i == 2)
+	  return 1;
+	  else
+	  return getFibo(i-1 ) + getFibo(i - 2);
+	  }
+
+	
+	 public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		 int n = sc.nextInt();
+	  System.out.println("斐波那契数列的前n项为：");
+	  fdnaq f = new fdnaq();
+	  for (int j = 0; j <= n; j++) {
+	   System.out.print(f.getFibo(j) + "\t");
+	   }
+	  }
+	 }
